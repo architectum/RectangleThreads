63079518ea27ac9a54caff7d26406b0330e1c774
SophieKolesnyk <sophiekolesnik@gmail.com>
2018-11-05 00:44:28 +0200

    windown rendering

--- RectanglesVCPP/main.cpp
+++ RectanglesVCPP/main.cpp
@@ -1,4 +1,6 @@
 #include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/button.hpp>
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
@@ -6,7 +8,19 @@ using namespace nana;
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	form f;
-	f.show();
+	form fm;
+	label lab{ fm, "It <bold>works</> :)" };
+	lab.format(true);
+	button btn{ fm, "Close" };
+	btn.events().click([&fm] {
+		fm.close();
+	});
+
+	fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
+	fm["text"] << lab;
+	fm["button"] << btn;
+	fm.collocate();
+
+	fm.show();
 	exec();
 }
